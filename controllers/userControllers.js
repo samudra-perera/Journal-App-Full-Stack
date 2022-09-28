37b2f3b6959432c310c3cbb0dce66fe9b8a57d55
Samudra Perera <samudrapup@gmail.com>
2022-09-27 21:02:09 -0400

    Added Login controller and routes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,11 +1,14 @@
 const User = require('../model/user')
+const bycrypt = require('bcryptjs')
 
 const signup = async(req, res, next) => {
     const {name, screenName, email, password} = req.body    //Destructure the req.body for cleaner code 
     let existingUser;
 
     try {
-        existingUser = await User.findOne({email: email});  //Finds if there is already a user with that same email 
+        existingUser = await User.findOne(
+            { $or: [{ email: email }, { userName: name }] }
+        );  //Finds if there is already a user with that same email 
     } catch (error) {
         console.log(error)
     }
@@ -13,11 +16,13 @@ const signup = async(req, res, next) => {
         return res.status(400).json({message: "User Already Exists Login Instead"})
     }
 
+    const hashedPassword = bycrypt.hashSync(password)
+
     const user = new User({ //Creating a new instance of the User model
         userName: name,
         screenName: screenName,
         email: email,
-        password: password,
+        password: hashedPassword,
     })
 
     try {
@@ -29,4 +34,25 @@ const signup = async(req, res, next) => {
     return res.status(201).json({message:user})
 }
 
+const login = async(req, res, next) => {
+    const {email, password} = req.body
+    let existingUser;
+
+    try {
+        existingUser = await User.findOne({email: email})   //Check if the user exists in the database
+    } catch (error) {
+        return new Error(error)
+    }
+    if(!existingUser) {
+        return res.status(400).json({message: "User not found. Signup Please"})     //If user does not exist in DB
+    }
+
+    const reHashedPassword = bycrypt.compareSync(password, existingUser.password);  //comparing the hashed password to check they match 
+    if(!reHashedPassword) {
+        return res.status(400).json({message: "Invalid Password"})  //if passwords do not match 0
+    }
+    return res.status(200).json({message: 'Successfully Logged In'})    //If passwords match respond with login status 
+}
+
 exports.signup = signup
+exports.login = login
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { signup } = require('../controllers/userControllers')
+const { signup, login } = require('../controllers/userControllers')
 const router = express.Router()
 
 router.post('/signup', signup)
+router.post('/login', login)
 
 module.exports = router
